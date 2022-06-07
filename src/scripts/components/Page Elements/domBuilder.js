13b743282f50f5ffb4b2de5fa8104f816dcbba33
Shalane Proctor <shalane088@gmail.com>
2022-06-07 00:40:22 -0500

    Added Navbar boot strap and made buttons

---
+++ src/scripts/components/Page Elements/domBuilder.js
@@ -0,0 +1,13 @@
+import renderToDom from '../Utility/renderToDom';
+
+const domBuilder = () => {
+  const domString = `
+  <div id="navigation"></div>
+  <div id="main-container">
+    <div id="add-card"></div>
+    <div id="form-container"></div>
+  </div>`;
+  renderToDom('#app', domString);
+};
+
+export default domBuilder;
--- src/scripts/helpers/auth.js
+++ src/scripts/helpers/auth.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import loginButton from '../components/loginButton';
-import logoutButton from '../components/logoutButton';
+// import logoutButton from '../components/logoutButton';
 import firebaseConfig from '../../api/apiKeys';
+import startApp from '../components/Utility/startApp';
 
 const checkLoginStatus = () => {
   firebase.initializeApp(firebaseConfig);
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // person is logged in do something...
-      logoutButton();
+      startApp();
     } else {
       // person is NOT logged in
       loginButton();
